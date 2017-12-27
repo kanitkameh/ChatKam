70d8abdbe4034823db893fd3fa75f19ace17fb8d
Kamen Vakavchiev <kanitkameh@gmail.com>
2017-12-27 03:04:08 +0200

    added threads for every client in the server

--- src/Connection.java
+++ src/Connection.java
@@ -1,8 +1,31 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
 
-public class Connection {
-
-	public Connection() {
-		// TODO Auto-generated constructor stub
+public class Connection implements Runnable{
+	Socket socket;
+	public Connection(Socket socket) {
+		this.socket = socket;
 	}
 
+	@Override
+	public void run() {
+		printClientInput();
+	}
+	void printClientInput() {
+		InputStream input;
+		try {
+			input = socket.getInputStream();
+			int character;
+			character = input.read();
+			while(character != -1){
+				System.out.print((char)character);
+				character = input.read();
+			}
+		} catch (IOException e) {
+			System.out.println("Couldn't get input stream. Error:");
+			e.printStackTrace();
+		}
+		
+	}
 }
--- src/Server.java
+++ src/Server.java
@@ -19,30 +19,17 @@ public class Server {
 	void startListening() {
 		Socket newConnection;
 		try {
-			newConnection = serverSocket.accept();
-			System.out.println("New client has connected");
-			
-			printClientInput(newConnection);
+			while(true) { //server keeps waiting for connection till force stop
+				newConnection = serverSocket.accept();
+				System.out.println("New client has connected");
+				
+				new Thread(new Connection(newConnection)).start(); //starting thread for every new client
+			}
 		} catch (IOException e) {
 			System.out.println("Client has tried to connect. Error:");
 			e.printStackTrace();
 		}
 	}
 	
-	void printClientInput(Socket socket) {
-		InputStream input;
-		try {
-			input = socket.getInputStream();
-			int character;
-			character = input.read();
-			while(character != -1){
-				System.out.print((char)character);
-				character = input.read();
-			}
-		} catch (IOException e) {
-			System.out.println("Couldn't get input stream. Error:");
-			e.printStackTrace();
-		}
-		
-	}
+	
 }
