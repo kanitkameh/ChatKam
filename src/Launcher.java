3afb1ee1f178e6b94fe1ce9d0f6ecd2989062e5c
Kamen Vakavchiev <kanitkameh@gmail.com>
2017-12-28 01:46:25 +0200

    Added ability to set custom socket and serversocket ports

--- src/Launcher.java
+++ src/Launcher.java
@@ -9,13 +9,17 @@ public class Launcher {
 		Scanner input = new Scanner(System.in);
 		String order = input.nextLine();
 		if(order.equals("server")) {
-			System.out.println("Starting server...");
-			Server me = new Server(2704);
+			System.out.println("Enter port number:");
+			int port = input.nextInt();
+			System.out.println("Starting server on port "+port);
+			Server me = new Server(port);
 			me.startListening();
 		}else if(order.equals("client")) {
 			System.out.println("Enter IP to join: ");
 			order = input.nextLine();
-			Client me = new Client(order,2704);
+			System.out.println("Enter port number:");
+			int port = input.nextInt();
+			Client me = new Client(order,port);
 			System.out.println("Welcome to the chat! Type \"EXIT\" to close conneciton)");
 			do{
 				order = input.nextLine();
