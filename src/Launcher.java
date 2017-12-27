e231876d8ae0a64e79631c3184528231d010cd1d
Kamen Vakavchiev <kanitkameh@gmail.com>
2017-12-27 02:22:13 +0200

    Server initialization and connection handler added

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class Launcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Do you want a server or client?");
+		Scanner input = new Scanner(System.in);
+		String order = input.nextLine();
+		if(order.equals("server")) {
+			System.out.println("Starting server...");
+			Server me = new Server(2704);
+			me.startListening();
+		}else if(order.equals("client")) {
+			System.out.println("Enter IP to join: ");
+			Client me = new Client();
+		}else {
+			System.out.println("You have entered invalid input.");
+		}
+		input.close();
 	}
 
 }
--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,47 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.*;
 
 public class Server {
-
-	public Server() {
-		// TODO Auto-generated constructor stub
+	
+	ServerSocket serverSocket;
+	
+	public Server(int port) {
+		try {
+			serverSocket = new ServerSocket(port);
+			System.out.println("Server has been initialized successfully!");
+		} catch (IOException e) {
+			System.out.println("Couldn't initialize server. Error is:");
+			e.printStackTrace();
+		}
 	}
-
+	
+	void startListening() {
+		Socket newConnection;
+		try {
+			newConnection = serverSocket.accept();
+			System.out.println("New client has connected");
+			
+			printClientInput(newConnection);
+		} catch (IOException e) {
+			System.out.println("Client has tried to connect. Error:");
+			e.printStackTrace();
+		}
+	}
+	
+	void printClientInput(Socket socket) {
+		InputStream input;
+		try {
+			input = socket.getInputStream();
+			int character;
+			do{
+				character = input.read();
+				System.out.print((char)character);
+			}while(character != -1);
+		} catch (IOException e) {
+			System.out.println("Couldn't get input stream. Error:");
+			e.printStackTrace();
+		}
+		
+	}
 }
